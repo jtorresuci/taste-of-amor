bce4e6c6f4d9d4114da75d7837bf2e370a02ee70
Jose Torres <jtorresocc@gmail.com>
2023-03-06 13:46:30 -0800

    first commit.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./Homepage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
