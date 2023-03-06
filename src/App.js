024dfe584a25c1f5be15a0bdd4f66b6efd70c21f
Jose Torres <jtorresocc@gmail.com>
2023-03-06 14:28:36 -0800

    Added function to handle submit.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homepage from "./Homepage";
 import theme from "./theme";
+import CateringSchedule from "./components/CateringSchedule";
 
 function App() {
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <Navbar />
+        <CateringSchedule/>
         <Routes>
           <Route exact path="/" element={<Homepage />} />
         </Routes>
