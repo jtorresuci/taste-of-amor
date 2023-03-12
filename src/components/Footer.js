b9937853308ae6b40a71f6172b29b49220456943
Jose Torres <jtorresocc@gmail.com>
2023-03-12 01:17:39 -0800

    Made the footer logo redirect to homepage.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,63 @@
 import React from "react";
 import { Box, Container, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Footer({ logo }) {
   return (
     <Box sx={{ color: "black", paddingTop: 20 }}>
-      <Container sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", flexDirection: { xs: "column", sm: "row" } }}>
-        <Box sx={{ display: "column", alignItems: "center", marginBottom: { xs: 5, sm: 5 } }}>
-        <Typography variant="body1">Taste of Amor</Typography>
-        <Typography variant="body1">714-777-7777</Typography>
+      <Container
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          flexDirection: { xs: "column", sm: "row" },
+        }}
+      >
+        <Box
+          sx={{
+            display: "column",
+            alignItems: "center",
+            marginBottom: { xs: 5, sm: 5 },
+          }}
+        >
+          <Typography variant="body1">Taste of Amor</Typography>
+          <Typography variant="body1">714-777-7777</Typography>
         </Box>
-        <Box sx={{ display: "flex", alignItems: "center", flex: 1, justifyContent: "center", marginBottom: { xs: 2, sm: 0 } }}>
-          <img src={logo} alt="logo" style={{ height: 40 }} />
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flex: 1,
+            justifyContent: "center",
+            marginBottom: { xs: 2, sm: 0 },
+          }}
+        >
+        <Link to="/">
+        <img src={logo} alt="logo" style={{ height: 40 }} />
+
+        </Link>
         </Box>
         <Box sx={{ display: "flex", alignItems: "center" }}>
-  <a href="https://www.yelp.com/" target="_blank" rel="noopener noreferrer" style={{ margin: "0 10px" }}>
-    <i className="fab fa-yelp fa-lg" style={{ color: "black" }}></i>
-  </a>
-  <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" style={{ margin: "0 10px" }}>
-    <i className="fab fa-instagram fa-lg" style={{ color: "black" }}></i>
-  </a>
-</Box>
-
+          <a
+            href="https://www.yelp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ margin: "0 10px" }}
+          >
+            <i className="fab fa-yelp fa-lg" style={{ color: "black" }}></i>
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ margin: "0 10px" }}
+          >
+            <i
+              className="fab fa-instagram fa-lg"
+              style={{ color: "black" }}
+            ></i>
+          </a>
+        </Box>
       </Container>
     </Box>
   );
