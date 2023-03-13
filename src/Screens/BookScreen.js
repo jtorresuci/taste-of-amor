ae4aa3386373f7418282386903835922ad8a1d3b
Jose Torres <jtorresocc@gmail.com>
2023-03-12 23:48:31 -0700

    renamed datetime -> bookingform

--- src/Screens/BookScreen.js
+++ src/Screens/BookScreen.js
@@ -1,14 +1,13 @@
 import { Box } from "@mui/system";
 import { DatePicker } from "@mui/x-date-pickers";
 import React from "react";
-import DateTimeComponent from "../components/BookScreen/DateTimeComponent";
+import BookingForm from "../components/BookScreen/BookingForm";
 
 function BookScreen() {
   return (
     <div>
       <Box >
-        <DateTimeComponent />
-        {/* <ContactForm /> */}
+        <BookingForm />
       </Box>
     </div>
   );
